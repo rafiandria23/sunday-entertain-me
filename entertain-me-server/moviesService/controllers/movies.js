ba424b40aa828beae2c015345898b6a25b6201e5
Adam Rafiandri <rafiandria23@gmail.com>
2020-03-16 20:55:37 +0700

    - Added endpoint for seeds from TMDB.

--- entertain-me-server/moviesService/controllers/movies.js
+++ entertain-me-server/moviesService/controllers/movies.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const axios = require('axios').default;
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+
 class MovieController {
   static async create(req, res, next) {
     try {
@@ -81,6 +84,42 @@ class MovieController {
         { returnOriginal: true }
       );
       res.status(200).json({ result });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async seedMovies(req, res, next) {
+    try {
+      const db = req.db;
+      const {
+        data: { genres }
+      } = await axios.get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${TMDB_API_KEY}`
+      );
+      const {
+        data: { results }
+      } = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&region=US`
+      );
+
+      const movies = results.map(movie => {
+        const tags = movie.genre_ids.map(tag => {
+          return genres
+            .filter(genre => genre.id === tag)
+            .map(genre => genre.name)[0];
+        });
+        return {
+          title: movie.title,
+          overview: movie.overview,
+          poster_path: movie.poster_path,
+          popularity: movie.popularity,
+          tags
+        };
+      });
+
+      const result = await db.collection('Movies').insertMany(movies);
+      res.status(201).json({ result: movies });
     } catch (err) {
       next(err);
     }
--- entertain-me-server/moviesService/routes/movies.js
+++ entertain-me-server/moviesService/routes/movies.js
@@ -4,6 +4,7 @@ const movieRouter = require('express').Router();
 const MovieController = require('../controllers/movies');
 
 movieRouter.get('/', MovieController.findAll);
+movieRouter.get('/seed', MovieController.seedMovies);
 movieRouter.get('/:movieId', MovieController.findOne);
 movieRouter.post('/', MovieController.create);
 movieRouter.put('/:movieId', MovieController.update);

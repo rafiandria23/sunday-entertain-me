15d59b3f7111cb39f63cd22f4f5e34ed7041c662
Adam Rafiandri <rafiandria23@gmail.com>
2020-03-18 18:23:28 +0700

    Stil trying to connect to Docker MongoDB.'

--- entertain-me-server/microservices/movies/seed.js
+++ entertain-me-server/microservices/movies/seed.js
@@ -6,11 +6,17 @@ const url = `mongodb://localhost:27017`;
 
 const dbName = 'entertainme';
 
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
-client.connect(async err => {
-  const db = client.db(dbName);
-  await db.createCollection('Movies');
-  await db.createCollection('TV Series');
-  await client.close();
-});
+const seed = async () => {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    await db.createCollection('Movies');
+    await db.createCollection('TV Series');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+seed();
